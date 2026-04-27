3cf15fd3128b4eeb9d4c7dc13e6ca78bfb2c0c66
armyvan <rubiomurilloivan@gmail.com>
2026-04-27 13:49:31 +0200

    Añadiendo sistema keep alive para evitar suspension en render

--- bookcut/src/main/java/com/darkmatter/bookcut/BookcutApplication.java
+++ bookcut/src/main/java/com/darkmatter/bookcut/BookcutApplication.java
@@ -2,8 +2,10 @@ package com.darkmatter.bookcut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BookcutApplication {
 
 	public static void main(String[] args) {
