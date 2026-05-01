d607bdba9d707523a9eb1c91728289ce42e8e3f9
armyvan <rubiomurilloivan@gmail.com>
2026-05-01 19:18:44 +0200

    Blindando aplicacion

---
+++ backend/src/main/java/com/darkmatter/bookcut/config/ConfiguracionZonaHoraria.java
@@ -0,0 +1,15 @@
+package com.darkmatter.bookcut.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class ConfiguracionZonaHoraria {
+
+    @PostConstruct
+    public void inicializarZonaHoraria() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
+    }
+}
